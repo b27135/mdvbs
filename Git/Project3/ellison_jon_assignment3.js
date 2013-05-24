86d4aa357aad8d457f2b6504e3f6498a2f1ec34a
Jon Ellison <ellisonjon30@gmail.com>
2013-05-24 00:37:04 -0500

    .html update

--- Git/Project3/ellison_jon_assignment3.js
+++ Git/Project3/ellison_jon_assignment3.js
@@ -3,6 +3,18 @@
 //Jon Ellison
 //SDI1305
 //Project3
-//24May13
-//Creating Flow Charts
+//23May13
+//Objects
 
+
+var cube = {
+
+	"name": "Rubik"
+	"length": 5,
+	"width": 5,
+	"height": 5,
+	"isSquare": true
+
+}; // end cube object
+
+console.log( " The lengthof the cube is " + cube.length);
