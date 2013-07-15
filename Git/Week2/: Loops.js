75528057610597d153639601045bc06525bda2c6
Jon Ellison <ellisonjon30@gmail.com>
2013-07-15 15:33:00 -0500

    PuttingWeek2 Files in Place

---
+++ Git/Week2/: Loops.js
@@ -0,0 +1,36 @@
+// Loops; 
+// Author: 
+
+//while loop
+var bonab = 99;
+
+while (bonab > 0 ) {
+	console.log(bonab + " bottles of non-alchoholic beverage on the wall. ");
+	bonab--;
+};
+
+
+console.log("No more bottles of non-alchoholic beverage.");
+
+
+
+
+//for-loop
+
+for (var bonab = 99; bonab > 0; bonab--) {
+
+	console.log(bonab + " bottles of non-alchoholic beverage on the wall. ");
+
+
+};
+console.log(" No more bottles of non-alchoholic beverage. ");
+
+
+//nested variable
+
+for (var i = 0; i < 5; i++) {
+	for (var ii = 0; ii < 3; ii++) {
+
+	};
+
+};
